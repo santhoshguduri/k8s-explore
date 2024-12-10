df094ba7002a9eef170e681b8d4be424105a9914
Santhosh Guduri <santhosh@fuzia.com>
2024-12-10 17:54:54 +0530

    created new template code

--- src/routes/config.jsx
+++ src/routes/config.jsx
@@ -5,13 +5,8 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import ProtectedRoute from "./ProtectedRoute";
 import { Dashboard } from "../pages/Dashboard";
-import Home from "../pages/Home";
   
 export const router = createBrowserRouter([
-    {
-      path: "/home",
-      element: <Home />,
-    },
     {
       path: "/login",
       element: <Login />,
